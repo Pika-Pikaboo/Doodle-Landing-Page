45e35d00ae377affed25ca5f51c1d8c611aaebc0
pikaboo <stevinjack1ina1000@gmail.com>
2022-11-04 13:27:26 +0630

    Doodle Landing Page

---
+++ script.js
@@ -0,0 +1,20 @@
+const humans = document.getElementById("humans");
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: [0.2],
+};
+
+function callback(entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add(".enlarge");
+    } else {
+      entry.target.classList.remove(".enlarge");
+    }
+  });
+}
+
+const observer = new IntersectionObserver(callback, options);
+observer.observe(humans);
